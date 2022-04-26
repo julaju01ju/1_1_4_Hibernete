6b9529598750cc0444af672341ebfc6c4fdc9eca
julaju01ju <96378819+julaju01ju@users.noreply.github.com>
2022-04-26 19:56:59 +0900

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,7 +13,7 @@ public class Util {
     // реализуйте настройку соеденения с БД
     private static final String URL = "jdbc:mysql://localhost:3306/Hibernate";
     private static final String USERNAME = "root";
-    private static String PASSWORD = "root";
+    private static final String PASSWORD = "root";
     private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
